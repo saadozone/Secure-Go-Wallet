edee0cd01c02020950920eab3048827f4b7cdb5c
Saad Bin Noman <125872373+saadozone@users.noreply.github.com>
2024-07-05 11:55:04 +0530

    Update database_dump.sql

--- database_dump.sql
+++ database_dump.sql
@@ -2,8 +2,6 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 12.12 (Ubuntu 12.12-0ubuntu0.20.04.1)
--- Dumped by pg_dump version 12.12 (Ubuntu 12.12-0ubuntu0.20.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
